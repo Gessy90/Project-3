1b04be9b557947a0a26b09a35226f4d152b231ac
Gessy90 <gessicasantoro90@hotmail.it>
2019-02-18 13:25:23 +0000

    Updated port in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongoose.plugin(require('mongoose-unique-validator'))
 const bodyParser = require('body-parser')
 const routes = require('./config/routes')
 const errorHandler = require('./lib/errorHandler')
-const { dbURI } = require('./config/environment')
+const { dbURI, port } = require('./config/environment')
 const app = express()
 
 mongoose.connect(dbURI)
@@ -21,6 +21,6 @@ app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 app.use(errorHandler)
 
-app.listen(process.env.PORT, () => console.log(`Up and running on port ${process.env.PORT}`))
+app.listen(port, () => console.log(`Up and running on port ${port}`))
 
 module.exports = app
